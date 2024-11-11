1a986613f4ac3fb3cb50dad40286a9eb08b909f1
ValentinCabral <66043628+ValentinCabral@users.noreply.github.com>
2024-11-11 00:59:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3000', // Usa la URL de backend desde VITE_API_URL en producción o localhost en desarrollo
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')  // Asegúrate de que se reescriba correctamente
+        changeOrigin: true
       }
     }
   }
